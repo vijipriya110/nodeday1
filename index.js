bd876f988f8caaf6226508c41d3741cdb610f47e
vijipriya110 <vijipriya110@gmail.com>
2024-02-06 10:38:48 +0530

    done

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const express = require("express");
 const fs = require("fs");
 const path= require("path")
 const dirName = path.join(__dirname, "timeStamps")
+// const [ , ,arg1] = process.argv
+// console.log(process.argv)
 
 // initialize express server framework
 
@@ -12,7 +14,7 @@ const app = express();
 
 // first get server
 app.get("/",(req,res)=>{
-    res.send("hi i am working good");
+//     res.send("hi i am working good");
 })
 //second get server
 app.get("/date-time",(req,res)=>{
@@ -22,16 +24,25 @@ app.get("/date-time",(req,res)=>{
    console.log(currentTimeStamp)
 
    console.log(dirName)
+//    console.log(process.argv)
 
    let content = `The last updted timeStamp : ${currentTimeStamp}`
 
    fs.writeFile(`${dirName}/date-time.text`,content, (error)=>{
     if(error){
         res.send("error in the file")
-        return
+        
+    }else{
+        res.sendFile(path.join(dirName,"date-time.text"))
     }
-    res.sendFile(path.join(dirName,"date-time.text"))
+    
    })
+   fs.readdir(`${dirName}`, function(err, items) {
+    // console.log(items);
+    
+    for (var i=0; i<items.length; i++) {
+        console.log(items[i]);
+    }});
 })
 
 //listen to a server
